f4afdcebbab4879dbeb108d83a69133ac0c0f724
lcbjrrr <lcbj@cesar.school>
2020-07-14 17:48:15 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+a=1
+b=2
+ss=a+b
+console.log(ss)
+
+function soma( a, b){
+    s = a+b;
+    return s;
+}
+
+ss=soma(10,20)
+console.log(ss)
+
+function calc(f, a, b){
+    return f(a,b)
+}
+
+s = calc(soma,11,22)
+console.log(s)
